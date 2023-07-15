1ab74c8318d1e394a406d0282a18d9df7e5b98f2
Nahjisu <nahjisoo0122@gmail.com>
2023-07-15 16:12:56 +0900

    create detail component

--- src/components/ShoesComponent.tsx
+++ src/components/ShoesComponent.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { Shoes } from "../model/shoes";
 
 // props type
@@ -7,13 +8,17 @@ interface OwnProps {
 }
 
 const ShoesComponent: React.FC<OwnProps> = ({ shoes, index }) => {
+  const navigate = useNavigate();
   return (
     <div>
       <div className="text-center">
         <img
-          className="h-52"
+          className="h-52 cursor-pointer"
           src={"https://codingapple1.github.io/shop/shoes" + index + ".jpg"}
           alt=""
+          onClick={() => {
+            navigate(`/detail/${shoes.id}`);
+          }}
         />
         <h4>상품명 : {shoes.title}</h4>
         <p>상품설명 : {shoes.content}</p>
