8bd29ad48dcd099a1f2399581d2575ae94e4b426
Nahjisu <nahjisoo0122@gmail.com>
2023-07-16 18:25:42 +0900

    add firebase

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,7 +4,32 @@ const Cart: React.FC = () => {
   return (
     <div>
       <Menu />
-      Cart Page
+      <CartTable />
+    </div>
+  );
+};
+
+const CartTable = () => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <th>#</th>
+          <th>상품</th>
+          <th>변경</th>
+          <th>삭제</th>
+        </thead>
+        <tbody>
+          <td>1</td>
+          <td>shoes</td>
+          <td>
+            <button>변경하기</button>
+          </td>
+          <td>
+            <button>삭제하기</button>
+          </td>
+        </tbody>
+      </table>
     </div>
   );
 };
